bf053af822cec8eb4b3c1c934dc611e7d92371bf
Hakurunai <guillaumeal.pro@gmail.com>
2024-09-16 11:35:09 +0200

    Adding Random library

--- Cpp_Review/Cpp_Review/src/main.cpp
+++ Cpp_Review/Cpp_Review/src/main.cpp
@@ -1,7 +1,10 @@
 #include "General_Include.h"
 
+#include "Random.h"
+
 void main()
 {
+	Random::Print10();
 	int32 v{ 4'000 };
 	return;
 }
---
+++ Cpp_Review/Random/src/Random.h
@@ -0,0 +1,14 @@
+#ifndef RANDOM__H
+#define RANDOM__H
+
+#include <iostream>
+
+namespace Random
+{
+	void Print10()
+	{
+		std::cout << 10 << std::endl;
+	}
+}
+
+#endif // RANDOM__H
